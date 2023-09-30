1f2c342ea383e9dfb877f83b7b75cbea02bb591a
David Jaeyoon Lee <djlee1004@berkeley.edu>
2023-09-29 17:28:40 -0700

    added drag and drop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Board from './components/Board';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 function App() {
   return (
     <div className="App">
       <h1 className='title'>Checkers Game</h1>
-      <Board />
+      <DndProvider backend={HTML5Backend}>
+        <Board />
+      </DndProvider>
     </div>
   );
 }
